614a56ad90b79af274595a7063368ec9a588f95c
Bartłomiej Tartanus <bartektartanus@gmail.com>
2017-08-19 10:32:07 +0200

    Initial commit

---
+++ src/PierwszyProgram.java
@@ -0,0 +1,11 @@
+public class PierwszyProgram {
+
+    public static void main(String[] args) {
+        System.out.println("To ja! Witaj Świecie!");
+        System.out.println("To ja! Witaj Świecie!");
+        System.out.println("To ja! Witaj Świecie!");
+        System.out.println("To ja! Witaj Świecie!");
+        System.out.println("To ja! Witaj Świecie!");
+
+    }
+}
